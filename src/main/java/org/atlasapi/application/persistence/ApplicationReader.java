1c31b5983785b8e9a3b4102bcd89037cfbaf5114
Fred van den Driessche <fred@metabroadcast.com>
2010-10-08 09:52:49 +0100

    added applications method to ApplicationReader interface

--- src/main/java/org/atlasapi/application/persistence/ApplicationReader.java
+++ src/main/java/org/atlasapi/application/persistence/ApplicationReader.java
@@ -1,9 +1,12 @@
 package org.atlasapi.application.persistence;
 
+import java.util.Set;
+
 import org.atlasapi.application.model.Application;
 
 public interface ApplicationReader {
 
 	Application applicationFor(String slug);
+	Set<Application> applications();
 	
 }
--- src/main/java/org/atlasapi/application/persistence/MongoApplicationStore.java
+++ src/main/java/org/atlasapi/application/persistence/MongoApplicationStore.java
@@ -1,10 +1,16 @@
 package org.atlasapi.application.persistence;
 
+import java.util.Set;
+
 import org.atlasapi.application.model.Application;
 
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 
 public class MongoApplicationStore implements ApplicationPersistor, ApplicationReader {
 	
@@ -22,10 +28,20 @@ public class MongoApplicationStore implements ApplicationPersistor, ApplicationR
 	public Application applicationFor(String slug) {
 		return translator.fromDBObject(applications.findOne(new BasicDBObject(ApplicationTranslator.APPLICATION_SLUG_KEY, slug)));
 	}
+	
+	@Override
+	public Set<Application> applications() {
+		return ImmutableSet.copyOf(Iterables.transform(applications.find(), new Function<DBObject, Application>(){
+			@Override
+			public Application apply(DBObject dbo) {
+				return translator.fromDBObject(dbo);
+			}
+		}));
+	}
 
 	@Override
 	public void persist(Application application) {
 		applications.insert(translator.toDBObject(application));
 	}
-
+	
 }
