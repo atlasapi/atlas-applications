71cf7f9d3b5c32705d9e7bf8293a23030deb67e3
liamgh <liam@greenhughes.com>
2013-08-20 18:41:25 +0100

    Add last updated time to application object

--- src/test/java/org/atlasapi/application/v3/ApplicationTest.java
+++ src/test/java/org/atlasapi/application/v3/ApplicationTest.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.*;
 
 import org.atlasapi.application.v3.Application;
 import org.atlasapi.application.v3.ApplicationCredentials;
+import org.joda.time.DateTime;
 import org.junit.Test;
+
+import com.metabroadcast.common.time.DateTimeZones;
 
 
 public class ApplicationTest {
@@ -12,5 +15,14 @@ public class ApplicationTest {
     @Test
     public void shouldCreateApplication() {
         assertNotNull(Application.application("test-slug").withCredentials(new ApplicationCredentials("apiKey")).build());
+    }
+    
+    @Test
+    public void testLastUpdated() {
+        Application application = Application.application("test-slug").withCredentials(new ApplicationCredentials("apiKey")).build();
+        assertTrue(application.getLastUpdated().isBeforeNow());
+        DateTime fixed = new DateTime(DateTimeZones.UTC).withDate(2013, 12, 13).withTime(9, 10, 20, 0);
+        application = application.copy().withLastUpdated(fixed).build();
+        assertEquals(fixed, application.getLastUpdated());
     }
 }
