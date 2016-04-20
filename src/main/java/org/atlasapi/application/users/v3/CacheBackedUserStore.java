2bcc6056848dcca63a0555cedab38a90067a2a59
Dias Saparov <dias@metabroadcast.com>
2016-04-20 17:47:33 +0100

    added ability to fetch user by its email

--- src/main/java/org/atlasapi/application/users/v3/CacheBackedUserStore.java
+++ src/main/java/org/atlasapi/application/users/v3/CacheBackedUserStore.java
@@ -13,6 +13,7 @@ public class CacheBackedUserStore implements UserStore {
     private final UserStore delegate;
     private LoadingCache<UserRef, Optional<User>> userRefCache;
     private LoadingCache<Long, Optional<User>> idCache;
+    private LoadingCache<String, Optional<User>> emailCache;
 
     public CacheBackedUserStore(final UserStore delegate) {
         this.delegate = delegate;
@@ -22,17 +23,34 @@ public class CacheBackedUserStore implements UserStore {
                 return delegate.userForRef(key);
             }
         });
-        this.idCache = CacheBuilder.newBuilder().expireAfterAccess(10, TimeUnit.MINUTES).build(new CacheLoader<Long, Optional<User>>() {
-            @Override
-            public Optional<User> load(Long key) throws Exception {
-                return delegate.userForId(key);
-            }
-        });
+        this.idCache = CacheBuilder
+                .newBuilder()
+                .expireAfterAccess(10, TimeUnit.MINUTES)
+                .build(new CacheLoader<Long, Optional<User>>() {
+                    @Override
+                    public Optional<User> load(Long key) throws Exception {
+                        return delegate.userForId(key);
+                    }
+                });
+        this.emailCache = CacheBuilder
+                .newBuilder()
+                .expireAfterAccess(10, TimeUnit.MINUTES)
+                .build(new CacheLoader<String, Optional<User>>() {
+                    @Override
+                    public Optional<User> load(String email) throws Exception {
+                        return delegate.userForEmail(email);
+                    }
+                });
     }
     
     @Override
     public Optional<User> userForRef(UserRef ref) {
         return userRefCache.getUnchecked(ref);
+    }
+
+    @Override
+    public Optional<User> userForEmail(String email) {
+        return emailCache.getUnchecked(email);
     }
 
     @Override
@@ -45,6 +63,7 @@ public class CacheBackedUserStore implements UserStore {
         delegate.store(user);
         userRefCache.invalidate(user.getUserRef());
         idCache.invalidate(user.getId());
+        emailCache.invalidate(user.getEmail());
     }
 
 }
