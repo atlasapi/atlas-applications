c2e06fbea6cb5355514d0c5897987496f512839a
Thomas Maroulis <thomas@metabroadcast.com>
2016-06-16 11:38:54 +0100

    Refactor for code style

--- src/test/java/org/atlasapi/application/v3/MongoApplicationStoreTest.java
+++ src/test/java/org/atlasapi/application/v3/MongoApplicationStoreTest.java
@@ -1,26 +1,24 @@
 package org.atlasapi.application.v3;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import org.atlasapi.application.v3.Application;
-import org.atlasapi.application.v3.ApplicationConfiguration;
-import org.atlasapi.application.v3.ApplicationCredentials;
 import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.persistence.ids.MongoSequentialIdGenerator;
-import org.junit.Before;
-import org.junit.Test;
+
+import com.metabroadcast.common.net.IpRange;
+import com.metabroadcast.common.persistence.MongoTestHelper;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.metabroadcast.common.net.IpRange;
-import com.metabroadcast.common.persistence.MongoTestHelper;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MongoApplicationStoreTest {
 
@@ -36,7 +34,11 @@ public class MongoApplicationStoreTest {
 	
 	@Test
 	public void testApplicationPersists() {
-		Application app1 = Application.application("test1").withTitle("Test 1").withCredentials(creds).build();
+		Application app1 = Application
+				.application("test1")
+				.withTitle("Test 1")
+				.withCredentials(creds)
+				.build();
 
 		appStore.persist(app1);
 
@@ -48,8 +50,16 @@ public class MongoApplicationStoreTest {
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testPersitenceOfApplicationsWithSameSlugsFails() {
-	    Application app1 = Application.application("testa").withTitle("Test 1").withCredentials(creds).build();
-	    Application app2 = Application.application("testa").withTitle("Test 2").withCredentials(creds).build();
+	    Application app1 = Application
+			    .application("testa")
+			    .withTitle("Test 1")
+			    .withCredentials(creds)
+			    .build();
+	    Application app2 = Application
+			    .application("testa")
+			    .withTitle("Test 2")
+			    .withCredentials(creds)
+			    .build();
 
 	    appStore.persist(app1);
 		appStore.persist(app2);
@@ -62,9 +72,15 @@ public class MongoApplicationStoreTest {
 	
 	@Test
 	public void testConfigurationPublisherPersistence() {
-	    Application app1 = Application.application("testb").withTitle("Test 1").withCredentials(creds).build();
+	    Application app1 = Application
+                .application("testb")
+                .withTitle("Test 1")
+                .withCredentials(creds)
+                .build();
 		
-		ApplicationConfiguration config = new ApplicationConfiguration(ImmutableMap.of(Publisher.FIVE, SourceStatus.AVAILABLE_ENABLED), null);
+		ApplicationConfiguration config = new ApplicationConfiguration(
+                ImmutableMap.of(Publisher.FIVE, SourceStatus.AVAILABLE_ENABLED), null
+        );
 		
 		app1 = app1.copy().withConfiguration(config).build();
 		
@@ -77,20 +93,32 @@ public class MongoApplicationStoreTest {
 	
 	@Test
 	public void testCredentialAPIKeyPersistence() {
-	    Application app1 = Application.application("testc").withTitle("Test 1").withCredentials(creds).build();
+	    Application app1 = Application
+                .application("testc")
+                .withTitle("Test 1")
+                .withCredentials(creds)
+                .build();
 		
 		appStore.persist(app1);
 		
 		Optional<Application> retrieved = appStore.applicationFor("testc");
 		
-		assertEquals(app1.getCredentials().getApiKey(), retrieved.get().getCredentials().getApiKey());
+		assertEquals(
+                app1.getCredentials().getApiKey(),
+                retrieved.get().getCredentials().getApiKey()
+        );
 	}
 	
 	@Test
 	public void testCredentialIPAddressPersistence() throws UnknownHostException {
-	    Application app1 = Application.application("testd").withTitle("Test 1").withCredentials(creds).build();
+	    Application app1 = Application
+                .application("testd")
+                .withTitle("Test 1")
+                .withCredentials(creds)
+                .build();
 		
-		ApplicationCredentials credentials = app1.getCredentials().copyWithIpAddresses(ImmutableList.of(new IpRange(InetAddress.getLocalHost())));
+		ApplicationCredentials credentials = app1.getCredentials()
+                .copyWithIpAddresses(ImmutableList.of(new IpRange(InetAddress.getLocalHost())));
 		
 		app1 = app1.copy().withCredentials(credentials).build();
 		
@@ -98,26 +126,41 @@ public class MongoApplicationStoreTest {
 		
 		Optional<Application> retrieved = appStore.applicationFor("testd");
 		
-		assertEquals(app1.getCredentials().getIpAddressRanges(), retrieved.get().getCredentials().getIpAddressRanges());
+		assertEquals(
+                app1.getCredentials().getIpAddressRanges(),
+                retrieved.get().getCredentials().getIpAddressRanges()
+        );
 	}
 	
 	@Test
 	public void testGetApplicationByAPIKey() {
-	    Application app1 = Application.application("teste").withTitle("Test 1").withCredentials(creds).build();
+	    Application app1 = Application
+                .application("teste")
+                .withTitle("Test 1")
+                .withCredentials(creds)
+                .build();
 		
 		appStore.persist(app1);
 		
-		Optional<Application> retrieved = appStore.applicationForKey(app1.getCredentials().getApiKey());
+		Optional<Application> retrieved = appStore.applicationForKey(
+                app1.getCredentials().getApiKey()
+        );
 		
 		assertEquals(app1, retrieved.get());
 	}
 	
 	@Test
 	public void testDeerIdGeneration() {
-        Application app1 = Application.application("teste").withTitle("Test 1").withCredentials(creds).build();
+        Application app1 = Application
+                .application("teste")
+                .withTitle("Test 1")
+                .withCredentials(creds)
+                .build();
         appStore.persist(app1);
         
-        Optional<Application> retrieved = appStore.applicationForKey(app1.getCredentials().getApiKey());
+        Optional<Application> retrieved = appStore.applicationForKey(
+                app1.getCredentials().getApiKey()
+        );
         assertEquals(5000L, retrieved.get().getDeerId().longValue());
 	}
 }
