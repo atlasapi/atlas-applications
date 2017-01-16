334edd1a40049cf4f047e7709f7a7deb4abe2437
Jamie Coleman <jamiec@metabroadcast.com>
2017-01-16 14:23:38 +0000

    PR feedback

--- src/main/java/org/atlasapi/application/query/InvalidApiKeyException.java
+++ src/main/java/org/atlasapi/application/query/InvalidApiKeyException.java
@@ -5,12 +5,19 @@ import com.metabroadcast.applications.client.exceptions.ErrorCode;
 public class InvalidApiKeyException extends ApplicationFetchException {
     private static final long serialVersionUID = -8204400513571208163L;
     
-    public InvalidApiKeyException(String apiKey, ErrorCode errorCode) {
+    private InvalidApiKeyException(String apiKey, ErrorCode errorCode) {
         super(apiKey, errorCode);
     }
 
+    public static InvalidApiKeyException create(String apiKey, ErrorCode errorCode) {
+        return new InvalidApiKeyException(apiKey, errorCode);
+    }
     @Override
     public String getMessage() {
-        return "Error fetching application for apikey: " + this.getApiKey() + " - " + this.getErrorCode();
+        return String.format(
+                "Error fetching application for api key: %s - %s",
+                this.getApiKey(),
+                this.getErrorCode()
+        );
     }
 }
