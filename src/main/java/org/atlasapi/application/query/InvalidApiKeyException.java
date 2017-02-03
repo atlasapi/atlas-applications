6d4d6d072a0a7859771967efe56bb9500e886df7
jamiesprax <jamiec@metabroadcast.com>
2017-02-03 12:02:03 +0000

    Revert "Revert "Mbst 17375 migrate to applications service client""

---
+++ src/main/java/org/atlasapi/application/query/InvalidApiKeyException.java
@@ -0,0 +1,23 @@
+package org.atlasapi.application.query;
+
+import com.metabroadcast.applications.client.exceptions.ErrorCode;
+
+public class InvalidApiKeyException extends ApplicationFetchException {
+    private static final long serialVersionUID = -8204400513571208163L;
+    
+    private InvalidApiKeyException(String apiKey, ErrorCode errorCode) {
+        super(apiKey, errorCode);
+    }
+
+    public static InvalidApiKeyException create(String apiKey, ErrorCode errorCode) {
+        return new InvalidApiKeyException(apiKey, errorCode);
+    }
+    @Override
+    public String getMessage() {
+        return String.format(
+                "Invalid api key: %s - %s",
+                this.getApiKey(),
+                this.getErrorCode()
+        );
+    }
+}
